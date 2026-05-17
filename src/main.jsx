52a515ec9f0038446a5d1af5ca4c86ff6540b00a
Shafi810 <shafek790@gamil.com>
2026-05-18 04:40:08 +0500

    Add Google Analytics initialization to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 1. Google Analytics ka package import karein
+import ReactGA from "react-ga4";
+
+// 2. Vercel/.env se ID read karein
+const trackingId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+// 3. Analytics ko initialize karein
+if (trackingId) {
+  ReactGA.initialize(trackingId);
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
